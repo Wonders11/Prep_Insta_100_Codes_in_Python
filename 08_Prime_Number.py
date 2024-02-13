347079c42957dd3d2c2e4b7940e686926c5455c4
Mohit_100 <mohithedaoo2000@gmail.com>
2024-02-13 19:36:02 +0530

    prime number

---
+++ 08_Prime_Number.py
@@ -0,0 +1,12 @@
+num = int(input("Enter the number: "))
+count = 0
+
+for i in range(1,num+1):
+    if num%i == 0:
+        count += 1
+
+if count<=2:
+    print("Prime Number")
+else:
+    print("Not a Prime Number")
+    
